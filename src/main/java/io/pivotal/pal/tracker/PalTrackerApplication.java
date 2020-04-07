06b192cdff43afaa44631405e98da061a08f8610
eliasrobin <robin.elias@cognizant.com>
2020-04-07 20:50:27 +0530

    Spring JDBC

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.util.TimeZone;
+
+import static java.lang.System.*;
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -16,9 +21,23 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
+    }*/
+    
+    /**
+     * Specifies desired TimeEntryRepository implementation, injected as needed by Spring
+     */
+    @Bean
+    TimeEntryRepository timeEntryRepository() {
+        if(getenv("ENV_NAME") != null && getenv("ENV_NAME").equalsIgnoreCase("test")) {
+            return new InMemoryTimeEntryRepository();
+        }
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
